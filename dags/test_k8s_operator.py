11dbcab6e28e6bc8c8eadff702b75dfc23acd083
skysky229 <nguyendinhthienhuy@gmail.com>
2026-01-09 23:43:38 +0700

    Update dag

--- dags/test_k8s_operator.py
+++ dags/test_k8s_operator.py
@@ -8,12 +8,12 @@ default_args = {
     'start_date': datetime(2024, 1, 1),
 }
 
-ch_user_secret = k8s.V1SecretEnvSource(name='clickhouse-creds')
+ch_user_secret = k8s.V1SecretEnvSource(name='clickhouse')
 
 with DAG(
     'dbt_clickhouse_stg_users',
     default_args=default_args,
-    schedule_interval=None,
+    schedule=None,  
     catchup=False,
     tags=['dbt', 'clickhouse'],
 ) as dag:
@@ -24,8 +24,8 @@ with DAG(
         namespace='default',
         image='skysky229/dbt-clickhouse:latest',
         env_from=[k8s.V1EnvFromSource(secret_ref=ch_user_secret)],
-        image_pull_policy='IfNotPresent',
         arguments=['run', '--select', 'stg_users'],
-        is_delete_operator_pod=True,
         get_logs=True,
+        # Airflow 3 favors more explicit cleanup actions
+        on_finish_action='delete_pod', 
     )
