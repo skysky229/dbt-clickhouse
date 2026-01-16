4c94a770b3396c8cf26c3c52753f039adb58b79a
skysky229 <nguyendinhthienhuy@gmail.com>
2026-01-17 00:55:33 +0700

    Change db

--- dbt/snapshots/customer_360/orders_snapshot.sql
+++ dbt/snapshots/customer_360/orders_snapshot.sql
@@ -1,7 +1,8 @@
 {% snapshot orders_snapshot %}
 {{
      config(
-          target_schema='snapshots',
+          target_schema='thelook',
+          target_database='thelook',
           unique_key='order_id',
           strategy='check',
           check_cols=['status', 'updated_at']
