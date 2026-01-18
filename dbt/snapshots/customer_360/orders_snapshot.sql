1854028354f0ad31e9f5f80159c5e519048fb45a
skysky229 <nguyendinhthienhuy@gmail.com>
2026-01-18 18:33:16 +0700

    Change snapshot configuration

--- dbt/snapshots/customer_360/orders_snapshot.sql
+++ dbt/snapshots/customer_360/orders_snapshot.sql
@@ -2,7 +2,6 @@
 {{
      config(
           target_schema='thelook',
-          target_database='thelook',
           unique_key='order_id',
           strategy='check',
           check_cols=['status', 'updated_at']
